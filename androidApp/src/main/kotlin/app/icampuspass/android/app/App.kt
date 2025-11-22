29953a3eda00f198c6ea0150086287dbc93103f5
Winston Sung <winston21.sung@gmail.com>
2025-11-22 16:27:34 +0800

    Test logic and Android UI
    Change-Id: Icd2bdd002b1d8c37a78c2ef5e203265e8634f11e

--- androidApp/src/main/kotlin/app/icampuspass/android/app/App.kt
+++ androidApp/src/main/kotlin/app/icampuspass/android/app/App.kt
@@ -1,19 +1,102 @@
 package app.icampuspass.android.app
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Surface
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import app.icampuspass.android.app.views.screens.GreetingScreen
+import androidx.lifecycle.compose.dropUnlessResumed
+import androidx.navigation.compose.rememberNavController
+import app.icampuspass.android.app.views.composables.ModalDrawerSheet
+import app.icampuspass.android.app.views.navigation.NavHost
+import app.icampuspass.android.app.views.navigation.destinations.AboutScreenDestination
+import app.icampuspass.android.app.views.navigation.destinations.MainScreenDestination
+import app.icampuspass.android.app.views.navigation.destinations.SettingsScreenDestination
 import app.icampuspass.android.shared.views.theme.Theme
+import kotlinx.coroutines.launch
 
 @Composable
 fun App() {
     Theme {
         Surface(color = Color.Black) {
             Surface(modifier = Modifier.safeDrawingPadding()) {
-                GreetingScreen()
+                val scope = rememberCoroutineScope()
+
+                val navController = rememberNavController()
+
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+                ModalNavigationDrawer(
+                    drawerContent = {
+                        ModalDrawerSheet(
+                            navController = navController,
+                            onMenuClose = {
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            onNavigateMain = dropUnlessResumed {
+                                scope.launch {
+                                    drawerState.close()
+                                }
+
+                                scope.launch {
+                                    navController.navigate(route = MainScreenDestination) {
+                                        popUpTo(route = MainScreenDestination)
+
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
+                                }
+                            },
+                            onNavigateSettings = dropUnlessResumed {
+                                scope.launch {
+                                    drawerState.close()
+                                }
+
+                                scope.launch {
+                                    navController.navigate(route = SettingsScreenDestination) {
+                                        popUpTo(route = MainScreenDestination)
+
+                                        launchSingleTop = true
+                                    }
+                                }
+                            },
+                            onNavigateAbout = dropUnlessResumed {
+                                scope.launch {
+                                    drawerState.close()
+                                }
+
+                                scope.launch {
+                                    navController.navigate(route = AboutScreenDestination) {
+                                        popUpTo(route = MainScreenDestination)
+
+                                        launchSingleTop = true
+                                    }
+                                }
+                            }
+                        )
+                    },
+                    drawerState = drawerState,
+                    content = {
+                        NavHost(
+                            scope = scope,
+                            navController = navController,
+                            drawerState = drawerState
+                        )
+                    }
+                )
+
+                BackHandler(enabled = drawerState.isOpen) {
+                    scope.launch {
+                        drawerState.close()
+                    }
+                }
             }
         }
     }
